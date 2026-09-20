c4db69ec4bd71d99c7bb877a8587870875a3e668
sh7vashrestha <shivashrestha927@gmail.com>
2026-09-20 00:36:33 -0400

    final

--- model/KMeans.py
+++ model/KMeans.py
@@ -11,24 +11,33 @@ def kmeans(data, k, max_iterations=100):
     centroids = data[indices].copy()
 
     for _ in range(max_iterations):
-        differences = data[:, np.newaxis] - centroids
-        distances = np.sqrt(
-            np.sum(differences ** 2, axis=2)
-        )
+        labels = []
 
-        labels = np.argmin(distances, axis=1)
+        for point in data:
+            distances = []
+            for centroid in centroids:
+                distance = np.sqrt(np.sum((point - centroid) ** 2))
+                distances.append(distance)
 
-        new_centroids = np.array([
-            data[labels == cluster].mean(axis=0)
-            if np.any(labels == cluster)
-            else centroids[cluster]
-            for cluster in range(k)
-        ])
+            labels.append(np.argmin(distances))
+
+        labels = np.array(labels)
+
+        new_centroids = []
+
+        for cluster in range(k):
+            cluster_points = data[labels == cluster]
+
+            if len(cluster_points) > 0:
+                new_centroids.append(cluster_points.mean(axis=0))
+            else:
+                new_centroids.append(centroids[cluster])
+
+        new_centroids = np.array(new_centroids)
 
         if np.allclose(centroids, new_centroids):
             centroids = new_centroids
             break
 
         centroids = new_centroids
-
     return labels, centroids
